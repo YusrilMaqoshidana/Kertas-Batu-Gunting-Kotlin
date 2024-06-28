bdb4c8cefe821793a6a4a2e3dfefad1cabc7dd69
yusril maqoshidana <yusrilkingsr07@gmail.com>
2024-06-28 15:58:32 +0700

    release: v2

--- app/src/main/java/id/usereal/rockpaperscissors/RockPaperScissors.kt
+++ app/src/main/java/id/usereal/rockpaperscissors/RockPaperScissors.kt
@@ -3,32 +3,40 @@ package id.usereal.rockpaperscissors
 fun main(){
     var computerChoice = ""
     var playerChoice = ""
-    println("Pilih Kertas, Batu, atau Gunting!")
-    print("Masukan pilihan anda : ")
-    playerChoice = readln()
-    val randomNumber = (1..3).random()
-    when (randomNumber) {
-        1 -> {
-            computerChoice = "Batu"
+    while (true){
+        println("Pilih Kertas, Batu, atau Gunting!")
+        print("Masukan pilihan anda : ")
+        playerChoice = readln()
+        val randomNumber = (1..3).random()
+        when (randomNumber) {
+            1 -> {
+                computerChoice = "Batu"
+            }
+            2 -> {
+                computerChoice = "Gunting"
+            }
+            3 -> {
+                computerChoice = "Kertas"
+            }
         }
-        2 -> {
-            computerChoice = "Gunting"
+        val winner = when{
+            playerChoice == computerChoice -> "Sama"
+            playerChoice == "Batu" && computerChoice == "Gunting" -> "Player"
+            playerChoice == "Gunting" && computerChoice == "Kertas" -> "Player"
+            playerChoice == "Kertas" && computerChoice == "Batu" -> "Player"
+            playerChoice == "Gunting" && computerChoice == "Batu" -> "Computer"
+            playerChoice == "Kertas" && computerChoice == "Gunting" -> "Computer"
+            playerChoice == "Batu" && computerChoice == "Kertas" -> "Computer"
+            playerChoice == "Break" -> return
+            else -> "Invalid"
         }
-        3 -> {
-            computerChoice = "Kertas"
-        }
-    }
-    val winner = when{
-        playerChoice == computerChoice -> "Sama"
-        playerChoice == "Batu" && computerChoice == "Gunting" -> "Player"
-        playerChoice == "Gunting" && computerChoice == "Kertas" -> "Player"
-        playerChoice == "Kertas" && computerChoice == "Batu" -> "Player"
-        else -> "Computer"
-    }
 
-    if (winner == "Sama"){
-        println("Hasilnya sama")
-    }else{
-        println(winner + " menang")
+        if (winner == "Sama"){
+            println("Hasilnya sama")
+        }else if(winner == "Invalid"){
+            println("Invalid choice")
+        } else{
+            println("$winner menang")
+        }
     }
 }
